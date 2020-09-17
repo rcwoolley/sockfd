34505c7f98f686bf1fd0d5b5909ae01fdb9cc49f
Rob Woolley <rob.woolley@windriver.com>
2020-09-17 17:41:12 -0400

    Add working recvfd
    Signed-off-by: Rob Woolley <rob.woolley@windriver.com>

--- recvfd.c
+++ recvfd.c
@@ -1,9 +1,57 @@
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include "scm_functions.h"
+
+#define BUFSIZE 100
 
 int main(int argc, char** argv) {
-	printf("Hello World\n");
-	return 0;
+    struct sockaddr_un addr;
+    char *socket_path = NULL;
+    char *default_sock = "socket";
+    char buf[BUFSIZE];
+    int sockfd, filefd, rc, bytes;
+
+    if (argc < 2) {
+        printf("Usage: recvfd <command> [SOCKET]\n");
+        exit(-1);
+    }
+
+    if (argc > 2) {
+	socket_path = argv[2];
+    } else {
+        socket_path = default_sock;
+    }
+
+    if ((sockfd = socket(AF_UNIX, SOCK_STREAM, 0)) == -1 ) {
+	perror("socket error");
+        exit(-1);
+    }
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, socket_path, sizeof(addr.sun_path)-1);
+
+    if (connect(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+        perror("connect error");
+        exit(-1);
+    }
+
+    if ((filefd = recvfd(sockfd)) == -1) {
+        printf("recvfd failed: %d\n", filefd);
+        exit(-1);
+    }
+
+    memset(buf, '\0', BUFSIZE);
+    while(bytes = read(filefd, buf, BUFSIZE-1) > 0) {
+        printf("<");
+        fprintf(stdout, buf, bytes);
+        printf(">\n");
+        memset(buf, '\0', BUFSIZE);
+    }
+
+    return 0;
 }
